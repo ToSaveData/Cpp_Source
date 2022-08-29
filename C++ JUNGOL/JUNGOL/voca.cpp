2484b77b72dafb1a0c70cfd8f4fd5d4529aa47d1
errandt <errandt@gmail.com>
2022-08-29 22:03:01 +0900

    C++

--- C++ JUNGOL/JUNGOL/voca.cpp
+++ C++ JUNGOL/JUNGOL/voca.cpp
@@ -2,40 +2,66 @@
 #include <sstream>
 using namespace std;
 
-string storage[100];
 
 int main()
 {
-
+	string storage[100];
 	string tmp[100];
+	string sentence;
+	string scan;
 	string input;
-	int cnt2 = 0, cnt1 = 0, flag = 0;
+	int cnt3 = 0, cnt2 = 0, cnt1 = 0;
+	
+	getline(cin, input);
+	if (input == "END")	return 0;
+	sentence = input;
 
+	stringstream SS(input);
+	for (; SS >> storage[cnt1]; cnt1++);
+	for (int i = 0; i < cnt1; i++)
+		cout << storage[i] << " ";
+	cout << endl;
+	SS.clear();
+	
 	do
 	{
 		getline(cin, input);
 		if (input == "END")	return 0;
+		scan = input;
 		stringstream SS(input);
-
 		for (; SS >> tmp[cnt2]; cnt2++);
-	
-		if (flag == 0)	for (; SS >> storage[cnt1]; cnt1++);
-
-		if (flag == 1)
+		for (int i = 0; i < cnt2; i++) 
 		{
-			for (int i = 0; i < cnt2; i++)
 			{
-				for (int j = 0; j < cnt1; j++)
-					if (storage[j] != tmp[i])
-						SS >> storage[cnt1++];
+				if (sentence.find(tmp[i]) == -1 || tmp[i] == "is")
+					storage[cnt1++] = tmp[i];
+				else if (sentence.find("is") != -1 && sentence.find(tmp[i]) == -1)
+					storage[cnt1++] = tmp[i]; 
 			}
+				/*if(sentence.find("is") == -1 && sentence.find(tmp[i]) == -1)
+					storage[cnt1++] = "is";					
+				else if (sentence.find("is") != -1 && tmp[i] == "is")
+					storage[cnt1++] = tmp[i];
+				else if (sentence.find("is") != -1 && sentence.find(tmp[i]) == -1)
+					storage[cnt1++] = tmp[i];*/
+				//else if(sentence.find("is") == -1 && sentence.find(tmp[i]) != -1)
+			cnt3++;
 		}
 
 		for (int i = 0; i < cnt1; i++)
 			cout << storage[i] << " ";
-		
+		if (cnt3 > 0)
+		{
+			for (int i = cnt3; i > 0; i--)
+			{
+				sentence = sentence.append(" ");
+				sentence = sentence.append(storage[cnt1 - i]);
+			}
+		}
+
 		cout << endl;
-		flag = 1;
+		SS.clear();
+		cnt3 = 0;
 	} while (1);
 
 	return 0;
